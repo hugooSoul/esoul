9d7cfea266aa596e2eabe02a32d26403cccd20c7
Hugo Lopez <hugol.soul@gmail.com>
2015-01-25 12:31:14 -0600

    Created esoul application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Esoul::Application.config.secret_key_base = '1106990bb255394efc4098083b651986b609226f4e31c93ca4441a07238bee195b54d2c1e9c901503a1e2e20a1a2718b3c719081935a0f25df1bd40ec3ada104'
