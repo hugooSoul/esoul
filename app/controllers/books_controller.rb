1788590e977a328efc5087c85b3a28b9676c9b5e
Hugo Lopez <hugol.soul@gmail.com>
2015-01-27 20:17:48 -0600

    Added book user association

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :edit,:create, :update, :destroy]
+    
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
     respond_to :html
@@ -22,7 +24,8 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
+    #@book = Book.new(book_params)
+      @book = current_user.books.new(book_params)
     @book.save
     respond_with(@book)
   end
